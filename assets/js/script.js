d755bb7d6bc5292f28eea8e45b9ae742fad760df
Leslie Figueroa <lesliek.x1999@gmail.com>
2024-06-09 22:44:43 -0400

    se crean archivos y se agrega ejercicio 1

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const imagen = document.getElementById('img');
+let contador = 0;
+
+imagen.addEventListener('click', ()=>{
+    if(contador==0){
+        imagen.classList.add('borde');
+        contador = 1;
+    }else{
+        imagen.classList.remove('borde');
+        contador = 0;
+    }
+
+} )
