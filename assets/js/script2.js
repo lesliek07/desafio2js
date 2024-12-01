585f4de73040077e135d32071fea52183840fe4e
Leslie Figueroa <lesliek.x1999@gmail.com>
2024-11-30 23:50:51 -0300

    mejora en codigo js ejercicio 2

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -8,14 +8,10 @@ let suma = 0;
 
 btn.addEventListener('click', ()=>{
     
-    let input1 = parseInt(document.getElementById('input1').value);
-    let input2 = parseInt(document.getElementById('input2').value);
-    let input3 = parseInt(document.getElementById('input3').value);
+    let input1 = parseInt(document.getElementById('input1').value) || 0;
+    let input2 = parseInt(document.getElementById('input2').value) || 0;
+    let input3 = parseInt(document.getElementById('input3').value) || 0;
     
-    input1 = input1 || 0;
-    input2 = input2 || 0;
-    input3 = input3 || 0;
-
     //console.log(input1, input2, input3)
     
     suma = input1 + input2+ input3;
@@ -24,14 +20,14 @@ btn.addEventListener('click', ()=>{
    
     if(suma <= 10){
         texto.innerHTML= 'Llevas ' + suma +' stickers';
-        document.getElementById('input1').value = '';
-        document.getElementById('input2').value = '';
-        document.getElementById('input3').value = '';
+        
     }else if(suma> 10){
         texto.innerHTML = 'Llevas demasiados stickers';
+
+    }
+
         document.getElementById('input1').value = '';
         document.getElementById('input2').value = '';
         document.getElementById('input3').value = '';
-    }
 
 })
